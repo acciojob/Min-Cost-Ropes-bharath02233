bd1fb5c1a5dabe02f0485dbb50b5544726b7691d
bharath02233 <bharath02233@gmail.com>
2025-02-21 14:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+   if (arr.length === 1) return 0; // Only one rope, no cost to connect
+
+    let minHeap = new MinHeap();
+    for (let num of arr) minHeap.insert(num);
+
+    let totalCost = 0;
+    while (minHeap.size() > 1) {
+        let first = minHeap.extractMin();
+        let second = minHeap.extractMin();
+        let cost = first + second;
+        totalCost += cost;
+        minHeap.insert(cost);
+    }
+
+    return totalCost;
   
 }
 
